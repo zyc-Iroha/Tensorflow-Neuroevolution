07e057eeac7f9720ea1f9624b0dd21e78e3d8ad0
Paul Pauls <mail@paulpauls.de>
2020-09-25 10:10:58 +0200

    Implement skeleton of DeepNEAT genome

--- tfne/encodings/deepneat/deepneat_genome.py
+++ tfne/encodings/deepneat/deepneat_genome.py
@@ -3,6 +3,7 @@ import json
 import tempfile
 import platform
 import subprocess
+from typing import Union
 
 import tensorflow as tf
 
@@ -52,12 +53,62 @@ class DeepNEATGenome(BaseGenome,
         self.genome_id = genome_id
         self.parent_mutation = parent_mutation
         self.generation = generation
-        self.optimizer = optimizer
         self.dtype = dtype
+
+        # Register genotype
+        self.genome_graph = genome_graph
+        self.preprocessing_layers = preprocessing_layers
+        self.output_layers = output_layers
+        self.optimizer = optimizer
 
         # Initialize internal variables
         self.fitness = None
 
         # Create model with genotype
         self.model = None
-        self._create_model(input_shape, genome_graph, preprocessing_layers, output_layers)
+        self._create_model(input_shape)
+
+    def __call__(self, inputs) -> tf.Tensor:
+        """"""
+        return self.model(inputs)
+
+    def __str__(self) -> str:
+        """"""
+        raise NotImplementedError("TODO")
+
+    def visualize(self, show=True, save_dir_path=None, **kwargs) -> str:
+        """"""
+        raise NotImplementedError("TODO")
+
+    def serialize(self) -> dict:
+        """"""
+        raise NotImplementedError("TODO")
+
+    def save_genotype(self, save_dir_path) -> str:
+        """"""
+        raise NotImplementedError("TODO")
+
+    def save_model(self, file_path, **kwargs):
+        """"""
+        self.model.save(filepath=file_path, **kwargs)
+
+    def set_fitness(self, fitness):
+        self.fitness = fitness
+
+    def get_genotype(self) -> ({int: Union[DeepNEATNode, DeepNEATConn]}, [dict], [dict], dict):
+        """"""
+        return self.genome_graph, self.preprocessing_layers, self.output_layers, self.optimizer
+
+    def get_model(self) -> tf.keras.Model:
+        """"""
+        return self.model
+
+    def get_optimizer(self) -> tf.keras.optimizers.Optimizer:
+        """"""
+        return tf.keras.optimizers.deserialize(self.optimizer)
+
+    def get_id(self) -> int:
+        return self.genome_id
+
+    def get_fitness(self) -> float:
+        return self.fitness
--- tfne/encodings/deepneat/deepneat_model.py
+++ tfne/encodings/deepneat/deepneat_model.py
@@ -2,6 +2,6 @@ import tensorflow as tf
 
 
 class DeepNEATModel:
-    def _create_model(self, input_shape, genome_graph, preprocessing_layers, output_layers):
+    def _create_model(self, input_shape):
         """"""
         pass
