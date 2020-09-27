d5a755c352c4768aa9b2f65c5ae99769abf055fc
Paul Pauls <mail@paulpauls.de>
2020-09-27 19:57:25 +0200

    Add full deepneat example config

--- tfne/populations/deepneat/deepneat_population.py
+++ tfne/populations/deepneat/deepneat_population.py
@@ -7,7 +7,19 @@ class DeepNEATPopulation(BasePopulation):
 
     def __init__(self, initial_state=None):
         """"""
-        pass
+        # Declare general internal variables of the CoDeepNEAT population
+        self.generation_counter = None
+        self.best_genome = None
+        self.best_fitness = None
+        self.best_consistent_genome = None
+        self.best_consistent_fitness = None
+
+        # Declare and initialize internal variables concerning the genome population of the CoDeepNEAT algorithm
+        self.genomes = dict()
+        self.species = dict()
+        self.species_repr = dict()
+        self.species_fitness_history = dict()
+        self.species_counter = 0
 
     def summarize_population(self):
         """"""
