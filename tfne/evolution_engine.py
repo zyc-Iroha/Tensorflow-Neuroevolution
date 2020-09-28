f3e499582691d0b552c30fc19c9c1219d28b0283
Paul Pauls <mail@paulpauls.de>
2020-09-28 22:02:21 +0200

    Adjust evolution engine to consider max_time

--- tfne/evolution_engine.py
+++ tfne/evolution_engine.py
@@ -1,6 +1,5 @@
 import os
 import ray
-import tensorflow as tf
 from datetime import datetime
 from tfne.encodings.base_genome import BaseGenome
 
@@ -8,8 +7,8 @@ from tfne.encodings.base_genome import BaseGenome
 class EvolutionEngine:
     """
     Central engine that drives the evolution by calling the evolutionary steps of the evolutionary algorithm in the
-    correct order while checking if the evolutionary process went extinct, reached its maximum fitness/generations or
-    is otherwise in order.
+    correct order while checking if the evolutionary process went extinct, reached its maximum fitness/generations/time
+    or is otherwise in order.
     """
 
     def __init__(self,
@@ -20,24 +19,28 @@ class EvolutionEngine:
                  max_fitness=None,
                  max_time=None):
         """
-        Creates a new evolutionary engine instance that initializes the multiprocessing library, creates TFNE state
-        backup directories and sets up evaluation environments in order to prepare for the eventual training process.
+        Creates a new evolutionary engine instance, creates TFNE state backup directories and sets up evaluation
+        environments in order to prepare for the eventual training process.
         @param ne_algorithm: instance of an TFNE compliant NE algorithm
         @param environment: instance of an TFNE compliant evaluation environment
         @param backup_dir_path: string of a directory path into which the TFNE state backups are saved
         @param max_generations: integer specifying the maximum number of generations the population should be evolved to
         @param max_fitness: float specifying the fitness of the best genome at which point the evolution process should
                             be preemptively stopped
+        @param max_time: timedelta specifying the maximum time that is allowed to pass until the neuroevolution process
+                         is aborted
         """
         # Register parameters and print evolution information
         self.ne_algorithm = ne_algorithm
         self.environment = environment
         self.max_generations = max_generations
         self.max_fitness = max_fitness
+        self.max_time = max_time
         print("Using Neuroevolution algorithm: {}".format(ne_algorithm.__class__.__name__))
         print("Using evaluation environment: {}".format(environment.__class__.__name__))
         print("Maximum number of generations to evolve the population: {}".format(max_generations))
         print("Maximum fitness value to evolve population up to: {}".format(max_fitness))
+        print("Maximum time to evolve population: {}".format(max_time))
 
         # Check if NE algorithm supports multiple evaluation instances and if so, duplicate the evaluation environment
         # for parallel evaluation.
@@ -60,17 +63,21 @@ class EvolutionEngine:
     def train(self) -> BaseGenome:
         """
         Starts the configured evolutionary training process. Initializes, then evaluates, summarizes and evolves
-        population in loop until exit condition (extinction, max generations/fitness reached) is met. Returns the genome
-        with the best achieved fitness
+        population in loop until exit condition (extinction, max generations/fitness/time reached) is met. Returns the
+        genome with the best achieved fitness
         @return: TFNE compliant genome with the best achieved fitness
         """
+        # Determine maximum datetime up to which the training process can proceed, if supplied
+        if self.max_time is None:
+            max_datetime = datetime.max
+        else:
+            max_datetime = datetime.now() + self.max_time
+
         # Initialize population. If pre-evolved population was supplied will it be used as the initial population.
-        # Pass the environment as a parameter to the initialization in order to allow the determination of constraints
-        # the created genomes have to adhere to.
-        self.ne_algorithm.initialize_population(self.environment)
+        self.ne_algorithm.initialize_population()
 
         # Start possibly endless training loop, only exited if population goes extinct, the maximum number of
-        # generations or the maximum fitness has been reached
+        # generations, the maximum fitness or the maximum time has been reached
         while True:
             # Evaluate and summarize population
             generation_counter, best_fitness = self.ne_algorithm.evaluate_population(self.environment)
@@ -79,13 +86,17 @@ class EvolutionEngine:
             # Backup population
             self.ne_algorithm.save_state(save_dir_path=self.backup_dir_path)
 
-            # Exit training loop if maximum number of generations or maximum fitness has been reached
+            # Exit training loop if maximum number of generations, maximum fitness or maximum time has been reached
             if self.max_fitness is not None and best_fitness >= self.max_fitness:
                 print("Population's best genome reached specified fitness threshold.\n"
                       "Exiting evolutionary training loop...")
                 break
             if self.max_generations is not None and generation_counter >= self.max_generations:
                 print("Population reached specified maximum number of generations.\n"
+                      "Exiting evolutionary training loop...")
+                break
+            if datetime.now() > max_datetime:
+                print("Neuroevolution reached specified maximum amount of time.\n"
                       "Exiting evolutionary training loop...")
                 break
 
@@ -97,10 +108,6 @@ class EvolutionEngine:
                 print("Population went extinct.\n"
                       "Exiting evolutionary training loop...")
                 break
-
-            # Reset models, counters, layers, etc including in the GPU to avoid memory clutter from old models as most
-            # likely only limited gpu memory is available
-            tf.keras.backend.clear_session()
 
         # Shutdown multiprocessing libraries now that training is ending
         ray.shutdown()
