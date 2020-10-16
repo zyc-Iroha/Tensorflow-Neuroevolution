16f70f04ca490fffa010b0c98f7f3090ab8f1045
Paul Pauls <mail@paulpauls.de>
2020-10-16 16:59:50 +0200

    Reset cyclic graph sorting algorithm, as it is erroneous

--- tests/playground.py
+++ tests/playground.py
@@ -148,11 +148,13 @@ min_graph_min_rec_conns = [([{0}, {2}, {5}, {8, 7}, {3}, {4, 6}, {1}], {(5, 2)})
 node_deps = node_dependencies.copy()
 node_deps[0] = set()
 
-recurrent_conns = set()
 graph_topology = list()
+recurrent_conns = set()
 
 while True:
-    print(f"node_deps: {node_deps}\n")
+    print(f"\ngraph_topology: {graph_topology}")
+    print(f"recurrent_conns: {recurrent_conns}")
+    print(f"node_deps: {node_deps}")
 
     # find all nodes in graph having no dependencies in current iteration
     dependencyless = set()
@@ -160,48 +162,19 @@ while True:
         if len(dep) == 0:
             dependencyless.add(node)
 
+    # Recurrency detected
     if not dependencyless:
-        if node_deps:
-            node_deps_min = None
-            node_deps_excluded = None
-            min_deps_count = sys.maxsize
+        node_deps_min = None
+        node_deps_min_itemcount = sys.maxsize
 
-            for node, node_deps_set in node_deps.items():
-                if node == 1:
-                    continue
-                if len(node_deps_set) < min_deps_count:
-                    min_deps_count = len(node_deps_set)
-                    node_deps_min = {node: node_deps_set}
-                    node_deps_excluded = {node}
-                elif len(node_deps_set) == min_deps_count:
-                    node_deps_min[node] = node_deps_set
-                    node_deps_excluded.add(node)
+        for node, node_deps_set in node_deps.items():
+            if len(node_deps_set) < node_deps_min_itemcount:
+                node_deps_min_itemcount = len(node_deps_set)
+                node_deps_min = {node: node_deps_set}
+            elif len(node_deps_set) == node_deps_min_itemcount:
+                node_deps_min[node] = node_deps_set
 
-            print("node_deps: {}".format(node_deps))
-            print("node_deps_min: {}".format(node_deps_min))
-            print("node_deps_excluded: {}".format(node_deps_excluded))
-
-            node_deps_min_adj = node_deps_min.copy()
-            for node, node_deps_min_set in node_deps_min.items():
-                new_node_deps_min_set = node_deps_min_set - node_deps_excluded
-                if not new_node_deps_min_set:
-                    del node_deps_min_adj[node]
-                else:
-                    node_deps_min_adj[node] = new_node_deps_min_set
-            if not node_deps_min_adj:
-                node_deps_min_adj = node_deps_min
-
-            print(f"node_deps_min_adj: {node_deps_min_adj}")
-
-            for node, node_deps_set in node_deps_min_adj.items():
-                for dep_nodes in node_deps_set:
-                    recurrent_conns.add((dep_nodes, node))
-                node_deps[node] = set()
-
-            print(f"node_deps: {node_deps}")
-            print(f"recurrent_conns: {recurrent_conns}\n")
-
-            continue
+        print(f"\nnode_deps_min: {node_deps_min}")
 
         break
 
@@ -212,132 +185,13 @@ while True:
     # dependencies of other nodes in order to create next iteration
     for node in dependencyless:
         del node_deps[node]
-    for node, dep in node_deps.items():
-        node_deps[node] = dep - dependencyless
 
-print(f"final graph_topology: {graph_topology}")
+    # if all nodes were removed from dependency list, exit topological sorting
+    if not node_deps:
+        break
+
+    for node, dependencies in node_deps.items():
+        node_deps[node] = dependencies - dependencyless
+
+print(f"\nfinal graph_topology: {graph_topology}")
 print(f"final recurrent_conns: {recurrent_conns}")
-
-exit()
-
-########################################################################################################################
-"""
-while True:
-    print(f"node_deps: {node_deps}\n")
-
-    # find all nodes in graph having no dependencies in current iteration
-    dependencyless = set()
-    for node, dep in node_deps.items():
-        if len(dep) == 0:
-            dependencyless.add(node)
-
-    if not dependencyless:
-        if node_deps:
-            node_deps_min = None
-            node_deps_excluded = None
-            min_deps_count = sys.maxsize
-
-            for node, node_deps_set in node_deps.items():
-                if node == 1:
-                    continue
-                if len(node_deps_set) < min_deps_count:
-                    min_deps_count = len(node_deps_set)
-                    node_deps_min = {node: node_deps_set}
-                    node_deps_excluded = {node}
-                elif len(node_deps_set) == min_deps_count:
-                    node_deps_min[node] = node_deps_set
-                    node_deps_excluded.add(node)
-
-            print("node_deps: {}".format(node_deps))
-            print("node_deps_min: {}".format(node_deps_min))
-            print("node_deps_excluded: {}".format(node_deps_excluded))
-
-            node_deps_min_adj = node_deps_min.copy()
-            for node in node_deps_min.keys():
-                node_deps_min_adj[node] -= node_deps_excluded
-
-            if not node_deps_min_adj:
-                node_deps_min_adj = node_deps_min
-            print(f"node_deps_min_adj: {node_deps_min_adj}")
-
-            '''
-            if len(node_deps_min_adj) > 1:
-                remaining_nodes = set(node_deps_min_adj.keys())
-                presence_count_in_other_deps = dict()
-                for node in remaining_nodes:
-                    other_remaining_nodes = remaining_nodes - {node}
-                    presence_count_in_other_deps[node] = 0
-                    for other_remaining_node in other_remaining_nodes:
-                        if node in node_deps_min_adj[other_remaining_node]:
-                            presence_count_in_other_deps[node] += 1
-    
-                node_to_clear = max(presence_count_in_other_deps.keys(), key=presence_count_in_other_deps.get)
-            else:
-                node_to_clear = next(iter(node_deps_min_adj))
-            '''
-            for node, node_deps_set in node_deps_min_adj.items():
-                for dep_nodes in node_deps_set:
-                    recurrent_conns.append((dep_nodes, node))
-                node_deps[node] = set()
-
-            print(f"node_deps: {node_deps}")
-            print(f"recurrent_conns: {recurrent_conns}")
-
-            print("")
-            continue
-
-        break
-
-        '''
-        if node_deps:
-            min_deps_nodes = None
-            min_deps_nodes_exclusion = None
-            min_deps_count = sys.maxsize
-            for node, node_deps_set in node_deps.items():
-                if len(node_deps_set) < min_deps_count:
-                    min_deps_nodes = node_deps_set
-                    min_deps_count = len(node_deps_set)
-                    if node not in node_deps_set:
-                        min_deps_nodes_exclusion = {node}
-                    else:
-                        min_deps_nodes_exclusion = set()
-                elif len(node_deps_set) == min_deps_count:
-                    min_deps_nodes = min_deps_nodes.union(node_deps_set)
-                    if node not in node_deps_set:
-                        min_deps_nodes_exclusion.add(node)
-
-            min_deps_nodes = min_deps_nodes - min_deps_nodes_exclusion
-            if not min_deps_nodes:
-                nodes_deps_len_dict = dict()
-                for node_deps_set in node_deps.values():
-                    if len(node_deps_set) == min_deps_count:
-                        if node_deps_set in nodes_deps_len_dict:
-                            nodes_deps_len_dict[node_deps_set] += 1
-                        else:
-                            nodes_deps_len_dict[node_deps_set] = 1
-                min_deps_nodes = min(nodes_deps_len_dict.keys(), key=nodes_deps_len_dict.get)
-
-            for node, node_deps_set in node_deps.items():
-                node_deps[node] = node_deps_set - min_deps_nodes
-                if not node_deps[node]:
-                    for dep_node in min_deps_nodes:
-                        recurrent_conns.append((dep_node, node))
-            continue
-        '''
-    # Add dependencyless nodes of current generation to list
-    graph_topology.append(dependencyless)
-
-    # remove keys with empty dependencies and remove all nodes that are considered dependencyless from the
-    # dependencies of other nodes in order to create next iteration
-    for node in dependencyless:
-        del node_deps[node]
-    for node, dep in node_deps.items():
-        node_deps[node] = dep - dependencyless
-
-print("RESULTS:")
-print(graph_topology)
-print(random_conns)
-print(recurrent_conns)
-print("EXIT")
-exit()
-"""
